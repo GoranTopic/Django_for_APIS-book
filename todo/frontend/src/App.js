e3bed624f68aa21599d44c2c478beb0e83904a63
telix <telegonicaxx@live.com>
2020-09-14 18:27:55 -0700

    checkpoint

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
 
+const initialState = {
+		list:[
+				{
+						"id":1,
+						"title":"1st todo",
+						"body":"Learn Django properly"
+				},
+				{
+						"id":2,
+						"title":"Second item",
+						"body":"Learn Python ",
+				},
+				{
+						"id":3,
+						"title":"Learn HTTP",
+						"body":"It's important",
+				}
+		],
+		isError: false,
+		isLoading: false,
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		
+		const reducer = (state, action) => {
+				switch(action.type){
+						case 'SET_LIST':
+								return { list: action.payload } 
+						case 'REMOVE_ITEM':
+								return { list: state.list.filter( item => (item.id === action.payload.id)? true : false  ) }
+						default:
+								console.log("No type found")
+								throw new Error()
+				}
+		}
+		const [ state, dispatcher ] = useReducer(reducer, initialState);
+
+
+		return (
+				<div className="App">
+						<header className="App-header">
+								{ state.list.map(item => (
+										<div key={item.id}>
+												<h1>{item.title}</h1>
+												<p>{item.body}</p>
+										</div>
+								))}
+						</header>
+				</div>
+		);
 }
 
 export default App;
