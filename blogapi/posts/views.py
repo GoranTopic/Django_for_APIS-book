0c2eebac848bed42837b03aebf51dfda6789e37a
telix <telegonicaxx@live.com>
2020-09-15 12:37:53 -0700

    checkpoint

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 # Create your views here.
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer   
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
+    permission_classes = (IsAuthorOrReadOnly,)
     serializer_class = PostSerializer   
