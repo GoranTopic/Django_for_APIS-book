7caad029cc767217c92315a61f5c01c27640e608
telix <telegonicaxx@live.com>
2020-09-14 19:09:25 -0700

    checkpoint

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect  } from 'react';
 import './App.css';
 
 const initialState = {
@@ -28,6 +28,12 @@ function App() {
 		
 		const reducer = (state, action) => {
 				switch(action.type){
+						case 'SET_IS_LOADING':
+								return { ...state, IsLoading: true, } 
+						case 'FETCH_SUCCESS':
+								return { list: action.payload, IsLoading: false, } 
+						case 'FETCH_ERROR':
+								return { list: [], IsLoading: false, isError:true,  } 
 						case 'SET_LIST':
 								return { list: action.payload } 
 						case 'REMOVE_ITEM':
@@ -37,21 +43,51 @@ function App() {
 								throw new Error()
 				}
 		}
+
+		// define dispatcher with the reducer 
 		const [ state, dispatcher ] = useReducer(reducer, initialState);
+
+
+
+		//definie use effecto load up the request 
+		useEffect(() => {
+				// Set State as loading
+				dispatcher({ type: 'SET_IS_LOADING' });
+				//fetch from API list
+				fetch('http://127.0.0.1:8000/api/')
+						.then( res => res.json() )
+						.then( res => dispatcher({ type:'FETCH_SUCCESS', payload: res.data }) )
+						.catch( () => dispatcher({ type:'FETCH_ERROR'  }))
+		}, [])
+		
+
+		
+
 
 
 		return (
 				<div className="App">
-						<header className="App-header">
-								{ state.list.map(item => (
-										<div key={item.id}>
-												<h1>{item.title}</h1>
-												<p>{item.body}</p>
-										</div>
-								))}
+						<header className="App-header">	
+								{ state.isError && <p>Something went wrong =(</p> }
+								{ (state.isLoading)? <p>Loading...</p> :  <List list={state.list} /> }
 						</header>
 				</div>
 		);
 }
+
+
+const List = ({ list, }) => {
+		return <>
+				{list.map(item => (
+						<div key={item.id}>
+								<h1>{item.title}</h1>
+								<p>{item.body}</p>
+						</div>
+				))}
+		</>
+
+}
+
+
 
 export default App;
