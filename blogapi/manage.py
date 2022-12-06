5aba5d40fae51ffb37bdb82071ac44c72dae973a
telix <telegonicaxx@live.com>
2022-12-06 02:18:18 -0800

    pr is failing but merging anyways, because I am an admin

--- blogapi/manage.py
+++ blogapi/manage.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-    try:
-        from django.core.management import execute_from_command_line
+
+
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+            "available on your PYTHONPATH environment variable? Did you "
+    """Run administrative tasks."""
+        from django.core.management import execute_from_command_line
+def main():
+"""Django's command-line utility for administrative tasks."""
+#!/usr/bin/env python
+import os
+        raise ImportError(
     main()
+import sys
+        ) from exc
+if __name__ == '__main__':
+            "Couldn't import Django. Are you sure it's installed and "
+    execute_from_command_line(sys.argv)
+    try:
