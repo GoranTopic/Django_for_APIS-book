3a4b1529d31d7025947225a43d93b69b8259bf66
telix <telegonicaxx@live.com>
2022-12-06 03:49:44 -0800

    i dunno, maybe this works

--- blogapi/manage.py
+++ blogapi/manage.py
@@ -1,22 +1,22 @@
-    main()
+#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-def main():
 import os
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-        from django.core.management import execute_from_command_line
-        raise ImportError(
-    try:
-    except ImportError as exc:
-            "available on your PYTHONPATH environment variable? Did you "
 import sys
 
 
-
-
-    execute_from_command_line(sys.argv)
+def main():
     """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-#!/usr/bin/env python
+            "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
